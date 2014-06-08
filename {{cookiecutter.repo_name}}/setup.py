2d77d93f1accbd78c22dc1bef3f498f7e0ebd5bc
Trey Hunner <trey@treyhunner.com>
2014-06-08 15:43:45 -0700

    Use latin1 encoding for Python files

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from {{ cookiecutter.repo_name }} import __version__
 
--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """
 test_{{ cookiecutter.repo_name }}
